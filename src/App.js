074d8bb9b5894730243544dbe15d639f4be91921
Sara El Badry <sara2lbadry@gmail.com>
2023-01-04 18:04:29 +0200

    creating components & pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Dashboard from "./pages/Dashboard/Dashboard"
+import Booking from "./pages/Booking/Booking"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/booking" element={<Booking />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Booking/Booking.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './booking.css'
+
+function Booking() {
+  return (
+    <div>Booking</div>
+  )
+}
+
+export default Booking
