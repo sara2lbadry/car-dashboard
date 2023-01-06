d48fbf8585ab5a44112ab0f6bbb09ed3b8bca788
Sara El Badry <sara2lbadry@gmail.com>
2023-01-06 20:02:22 +0200

    navbar jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './navbar.css'
+import {FiSearch} from "react-icons/fi"
+import {IoIosNotificationsOutline} from 'react-icons/io'
+import portrait from '../../assets/portrait.svg'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <section id='nav'>
+      <div className="navbar_container">
+        <div className="search_container">
+          <FiSearch className='sreach_icon' />
+          <input type="text" placeholder='Search or type' />
+        </div>
+        <div className='right'>
+          <IoIosNotificationsOutline className='notification_icon' />
+          <div className='portrait_img'>
+            <img src={portrait} alt="profile" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
