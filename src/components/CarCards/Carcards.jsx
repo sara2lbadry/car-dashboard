e6f0a67a0d1851dea2bff320e2be4b8dc27851d6
Sara El Badry <sara2lbadry@gmail.com>
2023-01-09 19:04:22 +0200

    one carCards component

--- src/components/CarCards/Carcards.jsx
+++ src/components/CarCards/Carcards.jsx
@@ -1,89 +1,47 @@
 import React from "react";
 import "./carcards.css";
 import { MdOutlineLoop } from "react-icons/md";
-import car1 from "../../assets/car1.svg";
-import car2 from "../../assets/carr.svg";
-import car3 from "../../assets/car3.svg";
 import { AiOutlineSetting } from "react-icons/ai";
 import { RiRepeatFill } from "react-icons/ri";
 import { SlEnergy } from "react-icons/sl";
 import { BsCurrencyDollar } from "react-icons/bs";
+import { carCards } from "../../data";
 
 function Carcards() {
+  console.log(carCards)
   return (
     <div className="card_container container">
       <div className="cars_cards">
-        <div
-          className="car_card"
-          style={{ backgroundColor: `var(--tertiary-secondary1)` }}
-        >
-          <div className="car_text">
-            <MdOutlineLoop />
-            <h3>64% Recommend</h3>
-          </div>
-          <img src={car1} />
-          <h2>Mini Cooper</h2>
-          <div className="car_stats">
-            <div className="carS">
-              <RiRepeatFill className="Ricon" />
-              <span>132K </span>
-              <AiOutlineSetting />
-              <SlEnergy />
-            </div>
-            <div className="carH">
-              <BsCurrencyDollar />
-              <span>32/h</span>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="car_card"
-          style={{ backgroundColor: `var(--tertiary-secondary2)` }}
-        >
-          <div className="car_text">
-            <MdOutlineLoop />
-            <h3>74% Recommend</h3>
-          </div>
-          <img src={car2} />
-          <h2>Porsche 911 Carrera</h2>
-          <div className="car_stats">
-            <div className="carS">
-              <RiRepeatFill className="Ricon" />
-              <span>130K </span>
-              <AiOutlineSetting />
-              <SlEnergy />
-            </div>
-            <div className="carH">
-              <BsCurrencyDollar />
-              <span>28/h</span>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="car_card"
-          style={{ backgroundColor: `var(--tertiary-secondary3)` }}
-        >
-          <div className="car_text">
-            <MdOutlineLoop />
-            <h3>74% Recommend</h3>
-          </div>
-          <img src={car3} />
-          <h2>Porsche 911 Carrera</h2>
-          <div className="car_stats">
-            <div className="carS">
-              <RiRepeatFill className="Ricon" />
-              <span>130K </span>
-              <AiOutlineSetting />
-              <SlEnergy />
-            </div>
-            <div className="carH">
-              <BsCurrencyDollar />
-              <span>28/h</span>
-            </div>
-          </div>
-        </div>
+        {carCards.map(({id, title, img, backgroundColor, type, rate, money}) => {
+          return (
+            <>
+              <div
+                key={id}
+                className="car_card"
+                style={{ backgroundColor:  backgroundColor }}
+              >
+                <div className="car_text">
+                  <MdOutlineLoop />
+                  <h3>{title}</h3>
+                </div>
+                <img src={img} />
+                <h2>{type}</h2>
+                <div className="car_stats">
+                  <div className="carS">
+                    <RiRepeatFill className="Ricon" />
+                    <span>{rate}</span>
+                    <AiOutlineSetting />
+                    <SlEnergy />
+                  </div>
+                  <div className="carH">
+                    <BsCurrencyDollar />
+                    <span>{money}</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,45 +1,42 @@
-import car1 from "./assets/car.svg"
-import car2 from "./assets/car2.svg"
-import car3 from "./assets/car3.svg"
-import car4 from "./assets/car4.svg"
-import car5 from "./assets/car5.svg"
-import car6 from "./assets/car6.svg"
+import car1 from "./assets/car.svg";
+import car2 from "./assets/car2.svg";
+import car3 from "./assets/car3.svg";
+import car4 from "./assets/car4.svg";
+import car5 from "./assets/car5.svg";
+import car6 from "./assets/car6.svg";
 import carCard1 from "./assets/car1.svg";
 import carCard2 from "./assets/carr.svg";
 import carCard3 from "./assets/car3.svg";
 
-
 const data = [
-    {
-        name: "1 PM",
-        stats: 4000,
-      },
-      {
-        name: "2 PM",
-        stats: 3000,
-      },
-      {
-        name: "3 PM",
-        stats: 5000,
-      },
-      {
-        name: "4 PM",
-        stats: 3000,
-      },
-      {
-        name: "5 PM",
-        stats: 3800,
-      },
-      {
-        name: "6 PM",
-        stats: 2000,
-      },
-      {
-        name: "7 Pm",
-        stats: 4000,
-      },
-    
- 
+  {
+    name: "1 PM",
+    stats: 4000,
+  },
+  {
+    name: "2 PM",
+    stats: 3000,
+  },
+  {
+    name: "3 PM",
+    stats: 5000,
+  },
+  {
+    name: "4 PM",
+    stats: 3000,
+  },
+  {
+    name: "5 PM",
+    stats: 3800,
+  },
+  {
+    name: "6 PM",
+    stats: 2000,
+  },
+  {
+    name: "7 Pm",
+    stats: 4000,
+  },
 ];
 export const carStatics = [
   {
@@ -80,94 +77,85 @@ export const carsModels = [
   {
     id: 1,
     carName: "Porshe 718 Cayman S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car1,
     people: 4,
     type: "Manual",
     money: 400,
-
   },
   {
     id: 2,
     carName: "Porshe 800 Cayman M",
-    amount : "Single",
+    amount: "Single",
     img: car2,
     people: 5,
     type: "Manual",
     money: 600,
-
   },
   {
     id: 3,
     carName: "Porshe 720 White S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car3,
     people: 4,
     type: "Manual",
     money: 550,
-
   },
   {
     id: 4,
     carName: "Kia 718 Blue S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car4,
     people: 4,
     type: "Manual",
     money: 440,
-
   },
   {
     id: 5,
     carName: "Porshe 718 Sliver S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car5,
     people: 4,
     type: "Manual",
     money: 800,
-
   },
   {
     id: 6,
     carName: "Porshe 718 Black S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car6,
     people: 6,
     type: "Manual",
     money: 400,
-
   },
   {
     id: 7,
     carName: "Porshe 718 Cayman S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car1,
     people: 4,
     type: "Manual",
     money: 400,
-
   },
   {
     id: 8,
     carName: "Porshe 718 Cayman S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car2,
     people: 4,
     type: "Manual",
     money: 400,
-
   },
   {
     id: 9,
     carName: "Porshe 718 Cayman S",
-    amount : "Coupe",
+    amount: "Coupe",
     img: car3,
     people: 4,
     type: "Manual",
     money: 400,
-
   },
-]
+];
 
 export const carCards = [
   {
@@ -178,7 +166,6 @@ export const carCards = [
     type: "Mini Cooper",
     rate: "132K",
     money: "32/h",
-
   },
   {
     id: 2,
@@ -188,7 +175,6 @@ export const carCards = [
     type: "Porsche 911 Carrera",
     rate: "130K",
     money: "28/h",
-
   },
   {
     id: 3,
@@ -198,8 +184,7 @@ export const carCards = [
     type: "Porsche 911 Carrera",
     rate: "130K",
     money: "28/h",
-
   },
-]
+];
 
-export default data ;
+export default data;
